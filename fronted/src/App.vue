09f6e082c19a2666ff7d808ff3f154ac62ac713e
akitomonam <akitomonam@gmail.com>
2022-12-23 16:03:44 +0900

    Vueファイルの追加(フロントエンド)

---
+++ fronted/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>ファイルアップロード</h1>
+    <form id="form" enctype="multipart/form-data" action="/upload" method="POST">
+      <input type="file" name="file" class="input file-input" multiple>
+      <button class="button" type="submit">アップロード</button>
+    </form>
+  </div>
+</template>
+
+<!-- <script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script> -->
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
