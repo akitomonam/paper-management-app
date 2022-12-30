8c50439c55c5b6ea9be7c8bd27a84bfce5b52cba
akitomonam <akitomonam@gmail.com>
2022-12-30 16:38:33 +0900

    ヘッダー修正

--- fronted/src/components/HeaderComponent.vue
+++ fronted/src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <header style="display: flex; align-items: center; justify-content: center; background-color: transparent">
         <img alt="MinamiLab logo" src="../assets/minami_lab_logo.png" style="height: 100px; width: 100px">
-        <h1 style="color: #000">Paper Management Site</h1>
+        <h1 style="color: #000">Paper Management App</h1>
     </header>
 </template>
 
