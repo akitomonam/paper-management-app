0f1abdb5a8ce643ea9357909310dd556ef19837f
akitomonam <akitomonam@gmail.com>
2023-01-10 01:02:11 +0900

    アップロードオーバーレイ追加

--- fronted/src/components/UploadForm.vue
+++ fronted/src/components/UploadForm.vue
@@ -1,29 +1,27 @@
 <template>
-    <form @submit.prevent="uploadFile">
-        <input type="file" ref="fileInput" @change="updateButtonDisabledStatus"/>
-        <button type="submit" :disabled="isButtonDisabled">Upload</button>
-        <!-- アップロード中のステータスを表す要素を表示する -->
-        <template v-if="isUploading">
-            <div class="uploading-status">
-                Uploading{{ '.'.repeat(uploadingStatusDots) }}
-            </div>
-        </template>
-    </form>
+    <div>
+        <form @submit.prevent="uploadFile">
+            <input type="file" ref="fileInput" @change="updateButtonDisabledStatus"/>
+            <button type="submit" :disabled="isButtonDisabled">Upload</button>
+        </form>
+        <vue-element-loading :active="isUploading" is-full-screen text="Now uploading..." size="128"/>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
 import { config } from '../../config';
+import VueElementLoading from "vue-element-loading";
 
 export default {
     name: 'UploadForm',
     components: {
+        VueElementLoading
     },
     data() {
         return {
             isButtonDisabled: true,
             isUploading: false,
-            uploadingStatusDots: 0,
         };
     },
     created() {
@@ -41,11 +39,6 @@ export default {
             }
 
             this.isUploading = true; // アップロード開始時に、アップロード中のステータスを表すデータプロパティを true に設定する
-            // アップロード中に "." の数を増やす処理を開始
-            this.uploadingStatusIntervalId = setInterval(() => {
-                // アップロード中に表示される "." の数を増やす
-                this.uploadingStatusDots = (this.uploadingStatusDots + 1) % 4;
-            }, 500);
             const file = this.$refs.fileInput.files[0];
             const formData = new FormData();
             formData.append('file', file);
@@ -64,9 +57,11 @@ export default {
                     // this.$emit('update-upload-status', response.data) // レスポンスボディのfilenameを参照して、responseMessageに代入する
                     this.$emit('update-upload-status') // レスポンスボディのfilenameを参照して、responseMessageに代入する
                 } else {
+                    this.isUploading = false; // アップロード完了後に、アップロード中のステータスを表すデータプロパティを false に設定する
                     console.error('レスポンスボディが存在しません')
                 }
             } catch (error) {
+                this.isUploading = false; // アップロード完了後に、アップロード中のステータスを表すデータプロパティを false に設定する
                 console.log('File uploaded Failed');
                 console.error(error);
             }
@@ -84,9 +79,9 @@ form {
     justify-content: center;
 }
 
-.uploading-status {
+/* .uploading-status {
     color: rgb(255, 0, 0);
-}
+} */
 
 /* アップロードボタンを装飾する */
 button[type="submit"] {
@@ -107,4 +102,74 @@ button[type="submit"][disabled] {
     background-color: #9e9e9e;
     cursor: not-allowed;
 }
+
+/* .loader,
+.loader:before,
+.loader:after {
+    background: #ffffff;
+    -webkit-animation: load1 1s infinite ease-in-out;
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+    height: 4em;
+}
+
+.loader {
+    color: #299ace;
+    text-indent: -9999em;
+    margin: 88px auto;
+    position: relative;
+    font-size: 11px;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+}
+
+.loader:before,
+.loader:after {
+    position: absolute;
+    top: 0;
+    content: '';
+}
+
+.loader:before {
+    left: -1.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+}
+
+.loader:after {
+    left: 1.5em;
+}
+
+@-webkit-keyframes load1 {
+
+    0%,
+    80%,
+    100% {
+        box-shadow: 0 0;
+        height: 4em;
+    }
+
+    40% {
+        box-shadow: 0 -2em;
+        height: 5em;
+    }
+}
+
+@keyframes load1 {
+
+    0%,
+    80%,
+    100% {
+        box-shadow: 0 0;
+        height: 4em;
+    }
+
+    40% {
+        box-shadow: 0 -2em;
+        height: 5em;
+    }
+} */
 </style>
